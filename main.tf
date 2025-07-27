7d2b7fb3a7e20327d53cfc605bffa8c47cab3d5a
Vikas Repo <Vikasvkulkarni@gmail.com>
2025-07-27 22:12:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "azurerm" {
   features {}
-  subscription_id = "289da116-5358-4835-8897-9e1db2cde3d2"
+  subscription_id = "661ea49e-05d4-429e-8204-de0d46c539b5"
 }
 
 resource "azurerm_resource_group" "rg-actions" {
